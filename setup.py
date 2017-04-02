459da1834d4cfca4f2687607797f1717dce3ee5e
Owen Synge <owen.synge@jaysnest.de>
2017-04-02 20:02:44 +0200

    move setup.py tests to pytest from nosetest

--- setup.py
+++ setup.py
@@ -10,6 +10,26 @@ else:
         	use_setuptools()
         	from setuptools import setup, find_packages
 
+
+
+from setuptools.command.test import test as TestCommand
+import sys
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def run_tests(self):
+        import shlex
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(shlex.split(self.pytest_args))
+        sys.exit(errno)
+
+
 name='dirvacua'
 setup(name=name,
     version=version,
@@ -30,19 +50,19 @@ setup(name=name,
         'Programming Language :: Python',
         ],
     scripts=['dirvacua'],
-    packages = ['pydirvacua'],
+    packages = ['pydirvacua', 'pydirvacua.tests'],
     data_files=[
         ('/usr/share/doc/%s' % (name),['README.md','ChangeLog','LICENSE'])
     ]
     ,
     tests_require=[
         'coverage >= 3.0',
-        'nose >= 1.1.0',
+        'pytest',
         'mock',
     ],
     setup_requires=[
         'nose',
     ],
-    test_suite = 'nose.collector',
+    cmdclass = {'test': PyTest},
     )
 
