5cba4628204c126147775deeabd0008594138a52
Owen Synge <owen.synge@jaysnest.de>
2019-04-09 00:13:25 +0200

    Move to new tox based setup

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from sys import version_info
 import os
 import os.path
-from pydirvacua.__version__ import version
-
 if version_info < (2, 6):
     print ("Please use a newer version of python")
     sys.exit(1)
@@ -38,21 +36,39 @@ def determine_path ():
         print ("There is no __file__ variable. Please contact the author.")
         sys.exit ()
 
+# Get version without importing the module.
+from distutils.util import convert_path
+
+main_ns = {}
+ver_path = convert_path('pydirvacua/__version__.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+
 from setuptools.command.test import test as TestCommand
 import sys
 
+
 class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
     def initialize_options(self):
         TestCommand.initialize_options(self)
-        self.pytest_args = []
+        self.pytest_args = ""
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
     def run_tests(self):
-        import shlex
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
+        try:
+            # import here, because outside the eggs aren't loaded
+            import pytest
+        except ImportError:
+            raise RuntimeError("py.test is not installed, "
+                               "run: pip install pytest")
+        errno = pytest.main([self.pytest_args])
         sys.exit(errno)
 
 needs_scripts = True
@@ -71,7 +87,7 @@ if "WITHOUT_DOC" in os.environ:
 name = "dirvacua"
 setup_args = {
     "name" : name,
-    "version" : version,
+    "version" : main_ns['version'],
     "description" : """dirvacua is a directory vacuuming tool.""",
     "long_description" : """Should you want to keep only the last "N" files of each type from a directory that contains files from multiple source deleteing based up on time may not be enough. dirvacua aims to clearly see which fiels are versions and only delete the older versioned files.""",
     "author" : "O M Synge",
@@ -103,7 +119,7 @@ if needs_jobs or needs_scripts or needs_docs:
     data_files = []
     path = determine_path ()
     if needs_docs is True:
-        installdir_doc = "/usr/share/doc/%s-%s" % (name,version)
+        installdir_doc = "/usr/share/doc/%s-%s" % (name, main_ns['version'])
         data_files.append((installdir_doc,['README.md','LICENSE','ChangeLog']))
     setup_args["data_files"] = data_files
 
