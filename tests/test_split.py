b68f3851d3c034c976be78710f03545665f46f85
Owen Synge <owen.synge@jaysnest.de>
2019-04-08 22:56:56 +0200

    pycodestyle: indentation

--- tests/test_split.py
+++ tests/test_split.py
@@ -7,8 +7,8 @@ def test_split_line_by_delimiter_with_regdelexp():
         ('1_2_3_4', ['1', '_', '2', '_', '3', '_', '4']),
         ('1.2.3.4', ['1', '.', '2', '.', '3', '.', '4']),
         ('1,2,3,4', ['1', ',', '2', ',', '3', ',', '4']),
-        ('1/2/3/4', ['1', '/', '2', '/', '3', '/', '4']),
-        ]
+        ('1/2/3/4', ['1', '/', '2', '/', '3', '/', '4'])
+    ]
     for data, expected in testdata:
         result = pydirvacua.dirvacua.split_line_by_delimiter(data, pydirvacua.dirvacua.regdelexp)
         assert (result == expected)
